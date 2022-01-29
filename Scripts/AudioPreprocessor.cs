a83110400db51aa90994922b9e78d01b65d7d58b
mikeyrev <33665661+mikeyrev@users.noreply.github.com>
2022-01-29 01:10:31 -0500

    Finished the tutorial
    Will add the visual stuff for preprocessing to make sure it works in the morning. I think I understand the process now and how we can look at individual frequency bins for analysis, which is next on the plate

--- Scripts/AudioPreprocessor.cs
+++ Scripts/AudioPreprocessor.cs
@@ -1,12 +1,11 @@
 //Functionality from https://medium.com/giant-scam/algorithmic-beat-mapping-in-unity-preprocessed-audio-analysis-d41c339c135a
 //Github Repo where code samples are pulled from: https://github.com/jesse-scam/algorithmic-beat-mapping-unity
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 using System.Numerics;
 using DSPLib;
+using System.Threading;
 
 public class AudioPreprocessor : MonoBehaviour
 {
@@ -15,17 +14,19 @@ public class AudioPreprocessor : MonoBehaviour
     private int numChannels;
     private int numTotalSamples;
     private float clipLength;
-    private float sampleRate;
+    private int sampleRate;
+    private SpectralFluxAnalyzer preProcessedSpectralFluxAnalyzer;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Init all the necessary data we need for the threaded FFT work
         init();
-        //Combine channels
-        float[] preProcessedSamples = new float[this.numTotalSamples];
-        combineChannels(preProcessedSamples);
 
-        //TODO: Replace "combine channels" step with getFullSpectrumThreaded using threads
+        //Begin thread to preprocess audio
+        Thread bgThread = new Thread(this.getFullSpectrumThreaded);
+        Debug.Log("Starting Background Thread");
+        bgThread.Start();
     }
 
     // Update is called once per frame
@@ -37,6 +38,7 @@ public class AudioPreprocessor : MonoBehaviour
     //Inits all member variables
     private void init()
     {
+        preProcessedSpectralFluxAnalyzer = new SpectralFluxAnalyzer();
         aud = GetComponent<AudioSource>();
         multiChannelSamples = new float[aud.clip.samples * aud.clip.channels];
         numChannels = aud.clip.channels;
@@ -45,10 +47,11 @@ public class AudioPreprocessor : MonoBehaviour
         sampleRate = aud.clip.frequency;
 
         aud.clip.GetData(multiChannelSamples, 0);
+        Debug.Log("GetData done");
     }
 
     //Combines the samples into one channel
-    private void combineChannels(float[] samples)
+    private void CombineChannels(float[] samples)
     {
         int numProcessed = 0;
         float combinedChannelAverage = 0f;
@@ -72,57 +75,11 @@ public class AudioPreprocessor : MonoBehaviour
         {
             // We only need to retain the samples for combined channels over the time domain
             float[] preProcessedSamples = new float[this.numTotalSamples];
-            combineChannels(preProcessedSamples);
-            //int numProcessed = 0;
-            //float combinedChannelAverage = 0f;
-            //for (int i = 0; i < multiChannelSamples.Length; i++)
-            //{
-            //    combinedChannelAverage += multiChannelSamples[i];
-
-            //    // Each time we have processed all channels samples for a point in time, we will store the average of the channels combined
-            //    if ((i + 1) % this.numChannels == 0)
-            //    {
-            //        preProcessedSamples[numProcessed] = combinedChannelAverage / this.numChannels;
-            //        numProcessed++;
-            //        combinedChannelAverage = 0f;
-            //    }
-            //}
-
+            CombineChannels(preProcessedSamples);
             Debug.Log("Combine Channels done");
             Debug.Log(preProcessedSamples.Length);
 
-            doFFT(preProcessedSamples);
-            //// Once we have our audio sample data prepared, we can execute an FFT to return the spectrum data over the time domain
-            //int spectrumSampleSize = 1024;
-            //int iterations = preProcessedSamples.Length / spectrumSampleSize;
-
-            //FFT fft = new FFT();
-            //fft.Initialize((UInt32)spectrumSampleSize);
-
-            //Debug.Log(string.Format("Processing {0} time domain samples for FFT", iterations));
-            //double[] sampleChunk = new double[spectrumSampleSize];
-            //for (int i = 0; i < iterations; i++)
-            //{
-            //    // Grab the current 1024 chunk of audio sample data
-            //    Array.Copy(preProcessedSamples, i * spectrumSampleSize, sampleChunk, 0, spectrumSampleSize);
-
-            //    // Apply our chosen FFT Window
-            //    double[] windowCoefs = DSP.Window.Coefficients(DSP.Window.Type.Hanning, (uint)spectrumSampleSize);
-            //    double[] scaledSpectrumChunk = DSP.Math.Multiply(sampleChunk, windowCoefs);
-            //    double scaleFactor = DSP.Window.ScaleFactor.Signal(windowCoefs);
-
-            //    // Perform the FFT and convert output (complex numbers) to Magnitude
-            //    Complex[] fftSpectrum = fft.Execute(scaledSpectrumChunk);
-            //    double[] scaledFFTSpectrum = DSPLib.DSP.ConvertComplex.ToMagnitude(fftSpectrum);
-            //    scaledFFTSpectrum = DSP.Math.Multiply(scaledFFTSpectrum, scaleFactor);
-
-            //    // These 1024 magnitude values correspond (roughly) to a single point in the audio timeline
-            //    float curSongTime = getTimeFromIndex(i) * spectrumSampleSize;
-
-            //    // Send our magnitude data off to our Spectral Flux Analyzer to be analyzed for peaks
-            //    preProcessedSpectralFluxAnalyzer.analyzeSpectrum(Array.ConvertAll(scaledFFTSpectrum, x => (float)x), curSongTime);
-            //}
-
+            DoFFT(preProcessedSamples);
             Debug.Log("Spectrum Analysis done");
             Debug.Log("Background Thread Completed");
 
@@ -134,8 +91,8 @@ public class AudioPreprocessor : MonoBehaviour
         }
     }
 
-    //Completes a Fast Fourier Transform (FFT) on a given array of 
-    private void doFFT(float[] samples)
+    //Completes a Fast Fourier Transform (FFT) on a given array of samples
+    private void DoFFT(float[] samples)
     {
         // Once we have our audio sample data prepared, we can execute an FFT to return the spectrum data over the time domain
         int spectrumSampleSize = 1024;
@@ -149,7 +106,7 @@ public class AudioPreprocessor : MonoBehaviour
         for (int i = 0; i < iterations; i++)
         {
             // Grab the current 1024 chunk of audio sample data
-            Array.Copy(preProcessedSamples, i * spectrumSampleSize, sampleChunk, 0, spectrumSampleSize);
+            Array.Copy(samples, i * spectrumSampleSize, sampleChunk, 0, spectrumSampleSize);
 
             // Apply our chosen FFT Window
             double[] windowCoefs = DSP.Window.Coefficients(DSP.Window.Type.Hanning, (uint)spectrumSampleSize);
@@ -162,10 +119,24 @@ public class AudioPreprocessor : MonoBehaviour
             scaledFFTSpectrum = DSP.Math.Multiply(scaledFFTSpectrum, scaleFactor);
 
             // These 1024 magnitude values correspond (roughly) to a single point in the audio timeline
-            float curSongTime = getTimeFromIndex(i) * spectrumSampleSize;
+            float curSongTime = GetTimeFromIndex(i) * spectrumSampleSize;
 
             // Send our magnitude data off to our Spectral Flux Analyzer to be analyzed for peaks
             preProcessedSpectralFluxAnalyzer.analyzeSpectrum(Array.ConvertAll(scaledFFTSpectrum, x => (float)x), curSongTime);
         }
     }
+
+    private int GetIndexFromTime(float curTime)
+    {
+        float lengthPerSample = this.clipLength / (float)this.numTotalSamples;
+
+        return Mathf.FloorToInt(curTime / lengthPerSample);
+    }
+
+    private float GetTimeFromIndex(int index)
+    {
+        return ((1f / (float)this.sampleRate) * index);
+    }
+
+
 }
